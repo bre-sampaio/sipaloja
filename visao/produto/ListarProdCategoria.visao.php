022657379e8cd9271fe168c497bce1339fb91059
1831062 <1831062@LABBMAQ09.itp.local>
2019-11-29 09:52:20 -0200

    csss

--- visao/produto/ListarProdCategoria.visao.php
+++ visao/produto/ListarProdCategoria.visao.php
@@ -1,35 +1,37 @@
+<div class="listaProdutos">
+<h2>Produtos por categoria</h2>
+ <div style="color: #d3d3d3; width: 100%; border: solid 1px;"></div>
 
-<h2>Lista de produtos por categoria</h2>
-<div id="listaProdutos">
     <?php
     foreach ($categorias as $categoria):
         ?>
-        <h2><?= $categoria['nome'] ?></h2>	
-        <div class="campos">
-            <div class="campoNomeProduto">
+    <h2 class="h22"><?= $categoria['nome'] ?></h2>	
+    
+     <div style="color: #d3d3d3; width: 100%; border: solid 1px;"></div>
+     
+        <div class="tabela1">
+            <div class="NomeProduto">
                 <h3>Produto</h3>
             </div>
-            <div class="campoPrecoProduto">
+            <div class="PrecoProduto">
                 <h3>Preço</h3>
             </div>
-            <div class="campoEstoqueProduto">
+            <div class="EstoqueProduto">
                 <h3>Estoque</h3>
             </div>
-            <div class="campoOperacoesProduto">
-                <h3>Operações</h3>
+            <div class="OperacoesProduto">
+                <h3>Opções</h3>
             </div>
         </div>        
-
-        <hr size="" width="100%">
+     <br><br><br>
         <?php
         foreach ($produtos as $produto):
-            if ($categoria["idCategoria"] == $produto["Categoria"]) {
-                ?>
-                <div class="cadaProduto">
-                    <div class="sobreNomeProduto">
-                        <div>
+            if ($categoria["idCategoria"] == $produto["Categoria"]) { ?>
+     
+                <div class="tabela2">
+                    
+                    <div class="nome">
                             <p><?= $produto['Nome'] ?></p>
-                        </div>
                     </div>
                     <div class="sobrePrecoProduto">
                         <p><?= $produto['Descricao'] ?></p>
@@ -46,7 +48,6 @@
 
                     </div>
                 </div>
-                <hr size="" width="100%">
                 <?php
             }
         endforeach;
@@ -56,3 +57,65 @@
 <a href="categoria/adicionar"><button class="botao2">Nova Categoria</button></a>
 <a href="produto/adicionar"><button class="botao2">Novo Produto</button></a><br><br>
 <a href="cliente/Adm"><button class="botao">Voltar</button></a><br><br>
+
+
+<style> 
+    .listaProdutos{
+        width: 40%;
+        margin: auto;
+    }
+    
+    .h22{
+        background-color: #d3d3d3;
+    }
+    
+    .tabela1{
+        display: inline;
+    }
+    
+    .NomeProduto{
+        width: 15%;
+        float: left;
+        margin-right: 6%;
+    }
+    
+    .PrecoProduto{
+        width: 15%;
+        float: left;
+         margin-right: 6%;
+    }
+    
+    .EstoqueProduto{
+        width: 15%;
+        float: left;
+         margin-right: 6%;
+    }
+    
+    .OperacoesProduto{
+        width: 15%;
+        float: left;
+    }
+    
+    .tabela2{
+        display: inline;
+    }
+    
+    .nome{
+       width: 15%;
+        float: left;
+    }
+    
+     .sobrePrecoProduto{
+       width: 15%;
+        float: left;
+    }
+    
+    .sobreEstoqueProduto{
+       width: 15%;
+        float: left;
+    }
+    
+    
+    
+    
+</style> 
